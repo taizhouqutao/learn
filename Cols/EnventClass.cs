d21ae38190a84d93257601c58e81db84eb0ba480
taizhouqutao <1026412418@qq.com>
2017-07-13 17:32:40 +0800

    text

---
+++ Cols/EnventClass.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace Cols
+{
+    public class EnventClass
+    {
+        public class CarInfoEventArgs : EventArgs
+        {
+            public CarInfoEventArgs(string car)
+            {
+                this.Car = car;
+            }
+            public string Car { get; set; }
+        }
+
+        public class CarDealer
+        {
+            public event EventHandler<CarInfoEventArgs> NewCarInfo;
+
+            public void NewCar(string car)
+            {
+                Console.WriteLine("CarDealer,new car {0}", car);
+
+                RaiseNewCarInfo(car);
+            }
+
+            public virtual void RaiseNewCarInfo(string car)
+            {
+                EventHandler<CarInfoEventArgs> newCarInfo = NewCarInfo;
+                if (newCarInfo != null)
+                {
+                    newCarInfo(this, new CarInfoEventArgs(car));
+                }
+            }
+        }
+
+        public class Consumer
+        {
+            private string name;
+
+            public Consumer(string name)
+            {
+                this.name = name;
+            }
+
+            public void NewCarIsHere(object sender, CarInfoEventArgs e)
+            {
+                Console.WriteLine("{0}: car {1} is new", name, e.Car);
+            }
+        }
+
+        public static void main(){
+            var dealer = new CarDealer();
+            var michael = new Consumer("Michael");
+            dealer.NewCarInfo += michael.NewCarIsHere;
+
+            dealer.NewCar("Ferrari");
+
+            var sebastian = new Consumer("Sebastian");
+            dealer.NewCarInfo += sebastian.NewCarIsHere;
+            dealer.NewCar("Mercedes");
+
+            dealer.NewCarInfo -= michael.NewCarIsHere;
+            dealer.NewCar("Red Bull Racing");
+
+            return;
+        }
+    }
+}
---
+++ Cols/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Cols
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");  
+
+            //Console.WriteLine(Test(Get,1,2));
+            EnventClass.main();
+        }
+
+        public static int Test(Func<int, int, int> func,int i,int j)
+        {
+            return func(i, j);
+        }
+
+        public static int test<T1,T2>(Func<T1, T2, int> func, T1 t1, T2 t2)
+        {
+            return func(t1, t2);
+        }
+
+        public static int Get(int i,int j)
+        {
+            return i + j;
+        }
+    }
+}
